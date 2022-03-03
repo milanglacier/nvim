a3038430997728757f72b7921b0e9862759c6962
northyear <zijz@qq.com>
2022-03-03 05:20:46 -0500

    Feat: Use shade.nvim to distinguish active window.

---
+++ lua/conf_shade.lua
@@ -0,0 +1,9 @@
+require'shade'.setup({
+  overlay_opacity = 70,
+  opacity_step = 1,
+  keys = {
+    brightness_up    = '<Leader><Leader><UP>',
+    brightness_down  = '<Leader><Leader><Down>',
+    toggle           = '<Leader><Leader>s',
+  }
+})
