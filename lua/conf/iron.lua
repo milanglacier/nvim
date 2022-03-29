64d8751f1de79638640947b14eb847039924d303
northyear <zijz@qq.com>
2022-03-29 03:24:01 -0400

    Feat: add bracketed-paste mode for radian

--- lua/conf/iron.lua
+++ lua/conf/iron.lua
@@ -1,33 +1,50 @@
+local iron = require 'iron'
+local mypath = require 'bin_path'
+
 vim.g.iron_map_defaults = 0
 vim.g.iron_map_extended = 0
 
-local iron = require("iron")
-local mypath = require('bin_path')
+require('iron.fts.python').ipython.command = mypath.ipython
+local extend = require("iron.util.tables").extend
 
-iron.core.add_repl_definitions{
+local format = function(open, close, cr)
+  return function(lines)
+    if #lines == 1 then
+      return { lines[1] .. cr }
+    else
+      local new = { open .. lines[1] }
+      for line=2, #lines do
+        table.insert(new, lines[line])
+      end
+      return extend(new, close)
+    end
+  end
+end
+
+iron.core.add_repl_definitions {
     r = {
         radian = {
-            command = {mypath.radian},
-        }
+            command = { mypath.radian },
+            format = format("\27[200~", "\27[201~\13", "\13"),
+        },
     },
 
     rmd = {
         radian = {
-            command = {mypath.radian},
-        }
+            command = { mypath.radian },
+            format = format("\27[200~", "\27[201~\13", "\13"),
+        },
     },
 }
 
-require('iron.fts.python').ipython.command = mypath.ipython
-
 iron.core.set_config {
     preferred = {
-        python = "ipython",
+        python = 'ipython',
         r = 'radian',
         rmd = 'radian',
-        clojure = "lein"
+        clojure = 'lein',
     },
-    repl_open_cmd = require('iron.view').openwin("belowright 15 split"),
+    repl_open_cmd = require('iron.view').openwin 'belowright 15 split',
     buflisted = true,
 }
 
