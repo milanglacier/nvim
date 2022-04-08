649b264f09a193b93d9c9599afdf7aa271da41fe
northyear <zijz@qq.com>
2022-04-08 01:43:13 -0400

    Feat: disable unused distribution plugins, add more treesitter parsers

--- lua/basic_settings.lua
+++ lua/basic_settings.lua
@@ -21,6 +21,26 @@ vim.o.clipboard = 'unnamedplus'
 vim.o.completeopt = 'menu,menuone,noselect'
 
 vim.g.did_load_filetypes = 1
+vim.g.did_load_fzf = 1
+vim.g.did_load_gtags = 1
+vim.g.did_load_gzip = 1
+vim.g.did_load_tar = 1
+vim.g.did_load_tarPlugin = 1
+vim.g.did_load_zip = 1
+vim.g.did_load_zipPlugin = 1
+vim.g.did_load_getscript = 1
+vim.g.did_load_getscriptPlugin = 1
+vim.g.did_load_vimball = 1
+vim.g.did_load_vimballPlugin = 1
+vim.g.did_load_matchit = 1
+vim.g.did_load_matchparen = 1
+vim.g.did_load_2html_plugin = 1
+vim.g.did_load_logiPat = 1
+vim.g.did_load_rrhelper = 1
+vim.g.did_load_netrw = 1
+vim.g.did_load_netrwPlugin = 1
+vim.g.did_load_netrwSettings = 1
+vim.g.did_load_netrwFileHandlers = 1
 
 if not vim.g.vscode then
     vim.o.mouse = 'a'
