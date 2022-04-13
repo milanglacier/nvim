56105066ef9379818221b3db31e8217bfb5607b9
northyear <zijz@qq.com>
2022-04-13 19:22:35 -0400

    Fix: nvim-cmp has a breaking change API.

--- lua/conf/cmp.lua
+++ lua/conf/cmp.lua
@@ -13,6 +13,20 @@ vim.cmd [[packadd! lspkind-nvim]]
 local cmp = require 'cmp'
 local lspkind = require 'lspkind'
 
+local my_mappings = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping {
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+    },
+    ['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<tab>'] = cmp.mapping.confirm { select = true },
+    ['<ESC>'] = cmp.mapping.abort(),
+}
+
 cmp.setup {
     snippet = {
         -- REQUIRED - you must specify a snippet engine
@@ -20,20 +34,7 @@ cmp.setup {
             require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
-    mapping = {
-
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ['<C-e>'] = cmp.mapping {
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        },
-        ['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<tab>'] = cmp.mapping.confirm { select = true },
-        ['<ESC>'] = cmp.mapping.abort(),
-    },
+    mapping = cmp.mapping.preset.insert(my_mappings),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'omni' },
@@ -65,6 +66,7 @@ cmp.setup.filetype('gitcommit', {
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(my_mappings),
     sources = {
         { name = 'buffer' },
     },
@@ -72,6 +74,7 @@ cmp.setup.cmdline('/', {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(my_mappings),
     sources = cmp.config.sources({
         { name = 'path' },
     }, {
