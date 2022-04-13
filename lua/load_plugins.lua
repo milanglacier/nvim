f239e05385805b195ec6da6b53a7f938525409af
northyear <zijz@qq.com>
2022-04-13 19:21:53 -0400

    Refactor: more organized plugins declarative.

--- lua/load_plugins.lua
+++ lua/load_plugins.lua
@@ -1,8 +1,10 @@
 require 'paq' {
 
+    -- optimize startup
     'lewis6991/impatient.nvim',
     'nathom/filetype.nvim',
 
+    -- colorschemes
     { 'EdenEast/nightfox.nvim', opt = true },
     { 'rose-pine/neovim', as = 'rose-pine', opt = true },
     { 'folke/tokyonight.nvim', branch = 'main', opt = true },
@@ -15,20 +17,27 @@ require 'paq' {
     -- Fix bugs and Utilities
     { 'antoinemadec/FixCursorHold.nvim' },
     { 'max397574/better-escape.nvim', opt = true },
+    { 'ygm2/rooter.nvim', opt = true },
+    { 'ahmedkhalf/project.nvim', opt = true },
+    { 'kyazdani42/nvim-tree.lua', opt = true }, -- file explorer
+    -- very simple, naive completion without LSP
+    -- Plug 'skywind3000/vim-auto-popmenu'
+    -- Plug 'skywind3000/vim-dict'
+    -- automatically switch input method when switch mode
+    { 'milanglacier/smartim', opt = true },
+    -- support browser
+    -- Plug 'glacambre/firenvim'
 
-    -- Set the theme for statusbar
+    -- UI
     { 'nvim-lualine/lualine.nvim', opt = true },
     { 'alvarosevilla95/luatab.nvim', opt = true },
     { 'rcarriga/nvim-notify', opt = true },
-    { 'echasnovski/mini.nvim', opt = true, branch = 'stable' },
+    { 'echasnovski/mini.nvim', opt = true },
+    { 'xiyaowong/nvim-transparent', opt = true },
+    { 'kyazdani42/nvim-web-devicons', opt = true },
 
-    -- automatically set the root;
-    { 'ygm2/rooter.nvim', opt = true },
-    { 'ahmedkhalf/project.nvim', opt = true },
-
-    -- Set the advanced text editing and jumping plug
+    -- text-editing, motions, jumps tools
     { 'justinmk/vim-sneak' },
-
     { 'tpope/vim-surround' },
     { 'numToStr/Comment.nvim' },
     { 'tpope/vim-repeat' },
@@ -42,6 +51,7 @@ require 'paq' {
     { 'D4KU/vim-textobj-chainmember' },
     { 'thinca/vim-textobj-between' },
 
+    -- text editing tools only for nvim
     { 'norcalli/nvim-colorizer.lua', opt = true },
     { 'windwp/nvim-autopairs', opt = true },
 
@@ -53,31 +63,25 @@ require 'paq' {
     { 'mizlan/iswap.nvim', opt = true },
     { 'romgrk/nvim-treesitter-context', opt = true },
 
-    -- Set markdown syntax highlighting
+    -- Set markdown/rmd syntax highlighting
     { 'vim-pandoc/vim-pandoc-syntax', opt = true },
     { 'vim-pandoc/vim-rmarkdown', branch = 'official-filetype', opt = true },
-    { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', opt = true },
 
     -- Fuzzy finder for file search
     { 'nvim-lua/plenary.nvim' },
     { 'nvim-telescope/telescope.nvim', opt = true },
     { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', opt = true },
 
-    -- very simple, naive completion without LSP
-    -- Plug 'skywind3000/vim-auto-popmenu'
-    -- Plug 'skywind3000/vim-dict'
-
-    -- Deal with input method, automatically changed to English
-    -- input method when switch to normal mode
-    { 'milanglacier/smartim', opt = true },
-    -- support browser
-    -- Plug 'glacambre/firenvim'
-
-    -- file explorer
-    { 'kyazdani42/nvim-tree.lua', opt = true },
-
     -- LSP config
     { 'neovim/nvim-lspconfig', opt = true },
+
+    -- lsp related tools, including lsp symbols, symbol outline, etc.
+    { 'tami5/lspsaga.nvim', opt = true },
+    { 'stevearc/aerial.nvim', opt = true },
+    { 'onsails/lspkind-nvim', opt = true },
+    { 'ray-x/lsp_signature.nvim', opt = true },
+    { 'jose-elias-alvarez/null-ls.nvim', opt = true },
+    { 'ThePrimeagen/refactoring.nvim', opt = true },
 
     -- Completion
     { 'hrsh7th/cmp-nvim-lsp', opt = true },
@@ -92,16 +96,10 @@ require 'paq' {
     { 'saadparwaiz1/cmp_luasnip', opt = true },
     { 'rafamadriz/friendly-snippets', opt = true },
 
-    -- Symbol Outline and signature help
-    { 'tami5/lspsaga.nvim', opt = true },
-    { 'stevearc/aerial.nvim', opt = true },
-    { 'onsails/lspkind-nvim', opt = true },
-    { 'ray-x/lsp_signature.nvim', opt = true },
-    { 'jose-elias-alvarez/null-ls.nvim', opt = true },
-    { 'ThePrimeagen/refactoring.nvim', opt = true },
-
     -- lua development
     { 'folke/lua-dev.nvim', opt = true },
+
+    -- CLI tools
 
     -- REPL
     -- 'jalvesaq/Nvim-R',
@@ -114,16 +112,15 @@ require 'paq' {
     { 'TimUntersberger/neogit', opt = true },
     { 'sindrets/diffview.nvim', opt = true },
 
+    -- Other cli tools, ripgrep, hover, markdown, etc
+    { 'windwp/nvim-spectre', opt = true },
+    { 'JASONews/glow-hover', opt = true },
+    { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', opt = true },
+
     -- Debugger
     { 'mfussenegger/nvim-dap', opt = true },
     { 'mfussenegger/nvim-dap-python', opt = true },
     { 'rcarriga/nvim-dap-ui', opt = true },
     { 'nvim-telescope/telescope-dap.nvim', opt = true },
     { 'theHamsta/nvim-dap-virtual-text', opt = true },
-
-    -- search and replace file
-    { 'windwp/nvim-spectre', opt = true },
-    { 'JASONews/glow-hover', opt = true },
-
-    { 'kyazdani42/nvim-web-devicons', opt = true },
 }
