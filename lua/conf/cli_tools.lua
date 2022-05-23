af825c21d4211b3e63f0f8731b5b51152522474b
northyear <zijz@qq.com>
2022-05-23 19:20:53 -0400

    Fix: update iron.nvim configs

--- lua/conf/cli_tools.lua
+++ lua/conf/cli_tools.lua
@@ -44,54 +44,26 @@ end
 M.load.iron = function()
     vim.cmd [[packadd! iron.nvim]]
 
-    local iron = require 'iron'
+    local iron = require 'iron.core'
     local mypath = require 'bin_path'
 
-    vim.g.iron_map_defaults = 0
-    vim.g.iron_map_extended = 0
+    local radian = require('iron.fts.r').radian
+    local ipython = require('iron.fts.python').ipython
+    radian.command = { mypath.radian }
+    ipython.command = { mypath.ipython }
 
-    require('iron.fts.python').ipython.command = mypath.ipython
-    local extend = require('iron.util.tables').extend
-
-    local format = function(open, close, cr)
-        return function(lines)
-            if #lines == 1 then
-                return { lines[1] .. cr }
-            else
-                local new = { open .. lines[1] }
-                for line = 2, #lines do
-                    table.insert(new, lines[line])
-                end
-                return extend(new, close)
-            end
-        end
-    end
-
-    iron.core.add_repl_definitions {
-        r = {
-            radian = {
-                command = { mypath.radian },
-                format = format('\27[200~', '\27[201~\13', '\13'),
+    iron.setup {
+        config = {
+            should_map_plug = true,
+            scratch_repl = true,
+            repl_definition = {
+                r = radian,
+                rmd = radian,
+                python = ipython,
             },
+            repl_open_cmd = 'belowright 15 split',
+            buflisted = true,
         },
-
-        rmd = {
-            radian = {
-                command = { mypath.radian },
-                format = format('\27[200~', '\27[201~\13', '\13'),
-            },
-        },
-    }
-
-    iron.core.set_config {
-        preferred = {
-            python = 'ipython',
-            r = 'radian',
-            rmd = 'radian',
-            clojure = 'lein',
-        },
-        repl_open_cmd = require('iron.view').openwin 'belowright 15 split',
-        buflisted = true,
     }
 
     keymap('n', '<localleader>rs', '<cmd>IronRepl<CR>', {})
