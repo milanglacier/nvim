cc5cacc01e02658ddabb3499ab5bebfff78fca58
northyear <zijz@qq.com>
2022-08-07 21:57:47 -0400

    Feat(packer): allow packer bootstrap

--- init.lua
+++ init.lua
@@ -1,5 +1,22 @@
-require('impatient').enable_profile()
+local packer_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local need_bootstrap = not pcall(require, 'packer')
+
+if need_bootstrap then
+    PACKER_BOOTSTRAP_SUCCESS =
+        vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', packer_path }
+    vim.cmd.packadd 'packer.nvim'
+    local autocmd = vim.api.nvim_create_autocmd
+    autocmd('User', {
+        pattern = 'PackerComplete',
+        command = 'quitall',
+    })
+else
+    require('impatient').enable_profile()
+end
 
 require 'basic_settings'
 require 'load_plugins'
-require 'load_configs'
+
+if not need_bootstrap then
+    require 'load_configs'
+end
