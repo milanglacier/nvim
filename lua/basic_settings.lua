ab5fed7241b7e0450a7f970380c575236b53aa71
northyear <zijz@qq.com>
2022-11-02 00:53:37 -0400

    config(basic_settings): set tabstop = 4

--- lua/basic_settings.lua
+++ lua/basic_settings.lua
@@ -10,6 +10,7 @@ vim.g.skip_defaults_vim = 1
 vim.o.number = true
 vim.o.autoindent = true
 vim.o.softtabstop = 4
+vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.showcmd = true
