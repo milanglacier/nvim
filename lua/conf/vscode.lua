419a13673e89c5619528da4437cd1ae598189d4c
northyear <zijz@qq.com>
2022-11-08 14:37:06 -0500

    refactor(vscode): use uinfied opts table for keymap

--- lua/conf/vscode.lua
+++ lua/conf/vscode.lua
@@ -5,6 +5,8 @@ local keymap = vim.api.nvim_set_keymap
 local autocmd = vim.api.nvim_create_autocmd
 local bufmap = vim.api.nvim_buf_set_keymap
 
+local opts = { silent = true }
+
 M.my_vscode = augroup('MyVSCode', {})
 
 vim.filetype.add {
@@ -23,35 +25,35 @@ local function v_notify(cmd)
     return string.format("<cmd>call VSCodeNotifyVisual('%s', 1)<CR>", cmd)
 end
 
-keymap('n', '<Leader>xr', notify 'references-view.findReferences', { silent = true }) -- language references
-keymap('n', '<Leader>xd', notify 'workbench.actions.view.problems', { silent = true }) -- language diagnostics
-keymap('n', 'gr', notify 'editor.action.goToReferences', { silent = true })
-keymap('n', '<Leader>ln', notify 'editor.action.rename', { silent = true })
-keymap('n', '<Leader>lf', notify 'editor.action.formatDocument', { silent = true })
-keymap('n', '<Leader>la', notify 'editor.action.refactor', { silent = true }) -- language code actions
+keymap('n', '<Leader>xr', notify 'references-view.findReferences', opts) -- language references
+keymap('n', '<Leader>xd', notify 'workbench.actions.view.problems', opts) -- language diagnostics
+keymap('n', 'gr', notify 'editor.action.goToReferences', opts)
+keymap('n', '<Leader>ln', notify 'editor.action.rename', opts)
+keymap('n', '<Leader>lf', notify 'editor.action.formatDocument', opts)
+keymap('n', '<Leader>la', notify 'editor.action.refactor', opts) -- language code actions
 
-keymap('n', '<Leader>fR', notify 'workbench.action.findInFiles', { silent = true }) -- use ripgrep to search files
-keymap('n', '<Leader>ts', notify 'workbench.action.toggleSidebarVisibility', { silent = true })
-keymap('n', '<Leader>th', notify 'workbench.action.toggleAuxiliaryBar', { silent = true }) -- toggle docview (help page)
-keymap('n', '<Leader>tp', notify 'workbench.action.togglePanel', { silent = true })
-keymap('n', '<Leader>fc', notify 'workbench.action.showCommands', { silent = true }) -- find commands
-keymap('n', '<Leader>ff', notify 'workbench.action.quickOpen', { silent = true }) -- find files
-keymap('n', '<Leader>tw', notify 'workbench.action.terminal.toggleTerminal', { silent = true }) -- terminal window
+keymap('n', '<Leader>fR', notify 'workbench.action.findInFiles', opts) -- use ripgrep to search files
+keymap('n', '<Leader>ts', notify 'workbench.action.toggleSidebarVisibility', opts)
+keymap('n', '<Leader>th', notify 'workbench.action.toggleAuxiliaryBar', opts) -- toggle docview (help page)
+keymap('n', '<Leader>tp', notify 'workbench.action.togglePanel', opts)
+keymap('n', '<Leader>fc', notify 'workbench.action.showCommands', opts) -- find commands
+keymap('n', '<Leader>ff', notify 'workbench.action.quickOpen', opts) -- find files
+keymap('n', '<Leader>tw', notify 'workbench.action.terminal.toggleTerminal', opts) -- terminal window
 
-keymap('n', '<Leader>mp', notify 'markdown.showPreviewToSide', { silent = true }) -- markdown preview
+keymap('n', '<Leader>mp', notify 'markdown.showPreviewToSide', opts) -- markdown preview
 
-keymap('v', '<Leader>lf', v_notify 'editor.action.formatSelection', { silent = true })
-keymap('v', '<Leader>la', v_notify 'editor.action.refactor', { silent = true })
-keymap('v', '<Leader>fc', v_notify 'workbench.action.showCommands', { silent = true })
+keymap('v', '<Leader>lf', v_notify 'editor.action.formatSelection', opts)
+keymap('v', '<Leader>la', v_notify 'editor.action.refactor', opts)
+keymap('v', '<Leader>fc', v_notify 'workbench.action.showCommands', opts)
 
 autocmd('FileType', {
     group = M.my_vscode,
     pattern = { 'r', 'rmd' },
     desc = 'set REPL keymaps for r, rmd',
     callback = function()
-        bufmap(0, 'n', '<LocalLeader>ss', notify 'r.runSelection', { silent = true })
-        bufmap(0, 'n', '<LocalLeader>sc', notify 'r.runCurrentChunk', { silent = true })
-        bufmap(0, 'n', '<LocalLeader>sgg', notify 'r.runAboveChunks', { silent = true })
+        bufmap(0, 'n', '<LocalLeader>ss', notify 'r.runSelection', opts)
+        bufmap(0, 'n', '<LocalLeader>sc', notify 'r.runCurrentChunk', opts)
+        bufmap(0, 'n', '<LocalLeader>sgg', notify 'r.runAboveChunks', opts)
     end,
 })
 
@@ -60,9 +62,9 @@ autocmd('FileType', {
     pattern = { 'python' },
     desc = 'set REPL keymaps for pythono',
     callback = function()
-        bufmap(0, 'n', '<LocalLeader>ss', notify 'jupyter.execSelectionInteractive', { silent = true })
-        bufmap(0, 'n', '<LocalLeader>sc', notify 'jupyter.runcurrentcell', { silent = true })
-        bufmap(0, 'n', '<LocalLeader>sgg', notify 'jupyter.runallcellsabove.palette', { silent = true })
+        bufmap(0, 'n', '<LocalLeader>ss', notify 'jupyter.execSelectionInteractive', opts)
+        bufmap(0, 'n', '<LocalLeader>sc', notify 'jupyter.runcurrentcell', opts)
+        bufmap(0, 'n', '<LocalLeader>sgg', notify 'jupyter.runallcellsabove.palette', opts)
     end,
 })
 
