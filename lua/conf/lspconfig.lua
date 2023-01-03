c1cf48d5493e459af8d5fa9bbd2d8a45ed4c4289
Milan Glacier <dev@milanglacier.com>
2023-01-03 04:52:52 -0500

    feat(lspconfig): add ltex-ls support.
    NOTE: ltex-ls uses non-standard code actions (i.e. add-to-dictionaries,
    hide-false-positives), which are not planned to be supported by
    nvim-lspconfig. The result is that ltex-ls are very likely to have a
    very high false positive rate to mark words as mispelled (which is
    supposed to be fixed by using the above code actions) and you can't fix
    them easily. There's workaround by using plugin: `ltex_extra.nvim` or
    `ltex-ls.nvim` to configure this lsp instead of nvim-lspconfig.
    Migrating to those plugins is to be considered.

--- lua/conf/lspconfig.lua
+++ lua/conf/lspconfig.lua
@@ -252,6 +252,31 @@ require('lspconfig').sqls.setup {
     end,
 }
 
+-- referenced from https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/ltex.lua
+local language_id_mapping = {
+    bib = 'bibtex',
+    plaintex = 'tex',
+    rnoweb = 'sweave',
+    rst = 'restructuredtext',
+    tex = 'latex',
+    xhtml = 'xhtml',
+    rmd = 'markdown',
+}
+
+require('lspconfig').ltex.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { 'bib', 'gitcommit', 'markdown', 'org', 'plaintex', 'rst', 'rnoweb', 'tex', 'rmd' },
+    get_language_id = function(_, filetype)
+        local language_id = language_id_mapping[filetype]
+        if language_id then
+            return language_id
+        else
+            return filetype
+        end
+    end,
+}
+
 vim.fn.sign_define('DiagnosticSignError', { text = '✗', texthl = 'DiagnosticSignError' })
 vim.fn.sign_define('DiagnosticSignWarn', { text = '!', texthl = 'DiagnosticSignWarn' })
 vim.fn.sign_define('DiagnosticSignInformation', { text = '', texthl = 'DiagnosticSignInfo' })
