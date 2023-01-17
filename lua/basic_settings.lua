d5b2690a89e6c39a2c10185e8b12223eef74bfaa
Milan Glacier <dev@milanglacier.com>
2023-01-16 20:20:46 -0500

    config: Don't disable builtin (distribution) plugins on startup.
    There's no need to disable builtin(distribution) plugins on startup,
    they have literally no influenec on startup speed.

--- lua/basic_settings.lua
+++ lua/basic_settings.lua
@@ -24,27 +24,6 @@ vim.o.clipboard = 'unnamedplus'
 vim.o.completeopt = 'menu,menuone,noselect'
 vim.o.tags = vim.o.tags .. ',.tags_columns'
 
-vim.g.did_load_fzf = 1
-vim.g.did_load_gtags = 1
-vim.g.did_load_gzip = 1
-vim.g.did_load_tar = 1
-vim.g.did_load_tarPlugin = 1
-vim.g.did_load_zip = 1
-vim.g.did_load_zipPlugin = 1
-vim.g.did_load_getscript = 1
-vim.g.did_load_getscriptPlugin = 1
-vim.g.did_load_vimball = 1
-vim.g.did_load_vimballPlugin = 1
-vim.g.did_load_matchit = 1
-vim.g.did_load_matchparen = 1
-vim.g.did_load_2html_plugin = 1
-vim.g.did_load_logiPat = 1
-vim.g.did_load_rrhelper = 1
-vim.g.did_load_netrw = 1
-vim.g.did_load_netrwPlugin = 1
-vim.g.did_load_netrwSettings = 1
-vim.g.did_load_netrwFileHandlers = 1
-
 vim.g.mapleader = ' '
 vim.g.maplocalleader = [[  ]]
 
