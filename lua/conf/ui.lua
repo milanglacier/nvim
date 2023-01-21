403b58d538a4667d9eaaf5c729bdfaec827b9891
Milan Glacier <dev@milanglacier.com>
2023-01-21 04:26:40 -0500

    config(lualine): use arc glyphs as separators.
    Sometimes you need a refresh, eh? Tired about arrow glyphs, isn't it?

--- lua/conf/ui.lua
+++ lua/conf/ui.lua
@@ -51,8 +51,8 @@ M.load.lualine = function()
         options = {
             icons_enabled = true,
             theme = 'auto',
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
             disabled_filetypes = {},
             always_divide_middle = false,
         },
