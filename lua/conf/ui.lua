c82d70de5d659b0bc0b5ee769c3baab870a5d867
Milan Glacier <dev@milanglacier.com>
2023-05-04 00:30:14 -0400

    fix(lualine): clean up code.

--- lua/conf/ui.lua
+++ lua/conf/ui.lua
@@ -87,7 +87,6 @@ M.load.lualine = function()
             theme = 'auto',
             component_separators = { left = '', right = '' },
             section_separators = { left = '', right = '' },
-            disabled_filetypes = {},
             always_divide_middle = false,
             disabled_filetypes = {
                 winbar = {
