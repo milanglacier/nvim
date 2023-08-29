902426fb2c0a9ffc5a921e03a252e6a2f116f3eb
Milan Glacier <dev@milanglacier.com>
2023-08-28 22:51:28 -0400

    fix(treesitter/injection): update SQL injection for treesitter 0.9 grammer.

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -3,16 +3,16 @@
 ; shout out to https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/ecma/injections.scm
 (string (string_content) @sql
         (#lua-match? @sql "^%s*--sql")
-        (#exclude_children! @sql))
+        ((#set! injection.language "sql")))
 
 (string (string_content) @sql
         (#lua-match? @sql "^%s*--SQL")
-        (#exclude_children! @sql))
+        ((#set! injection.language "sql")))
 
 (string (string_content) @sql
         (#lua-match? @sql "^%s*/\\*.*sql.*\\*/")
-        (#exclude_children! @sql))
+        ((#set! injection.language "sql")))
 
 (string (string_content) @sql
         (#lua-match? @sql "^%s*/\\*.*SQL.*\\*/")
-        (#exclude_children! @sql))
+        ((#set! injection.language "sql")))
