a7386a46e32802e3708b4b52779ee3fce38fa596
Milan Glacier <dev@milanglacier.com>
2023-08-29 16:43:07 -0400

    fix(treesitter/injection): update injection syntax.
    My familiarity with the schema dialect used in treesitter is quite
    limited. The modifications made were primarily derived from educated
    assumptions based on the code structure of `nvim-treesitter`.

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -1,18 +1,18 @@
 ; extends
 
 ; shout out to https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/ecma/injections.scm
-(string (string_content) @sql
-        (#lua-match? @sql "^%s*--sql")
-        ((#set! injection.language "sql")))
+(((string_content) @sql
+                   (#lua-match? @sql "^%s*--sql")) @injection.content
+ (#set! injection.language "sql"))
 
-(string (string_content) @sql
-        (#lua-match? @sql "^%s*--SQL")
-        ((#set! injection.language "sql")))
+(((string_content) @sql
+                   (#lua-match? @sql "^%s*--SQL")) @injection.content
+ (#set! injection.language "sql"))
 
-(string (string_content) @sql
-        (#lua-match? @sql "^%s*/\\*.*sql.*\\*/")
-        ((#set! injection.language "sql")))
+(((string_content) @sql
+                   (#lua-match? @sql "^%s*/\\*.*sql.*\\*/")) @injection.content
+ (#set! injection.language "sql"))
 
-(string (string_content) @sql
-        (#lua-match? @sql "^%s*/\\*.*SQL.*\\*/")
-        ((#set! injection.language "sql")))
+(((string_content) @sql
+                   (#lua-match? @sql "^%s*/\\*.*SQL.*\\*/")) @injection.content
+ (#set! injection.language "sql"))
