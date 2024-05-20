c77a641de63004bfe1e1c0c2983aea6fd7011574
Milan Glacier <dev@milanglacier.com>
2024-05-20 14:54:22 -0400

    config(dap): update `dap` hooks for toggling `dapui`.

--- lua/conf/dap.lua
+++ lua/conf/dap.lua
@@ -33,13 +33,16 @@ local function load_dap()
         },
     }
 
-    dap.listeners.after.event_initialized['dapui_config'] = function()
+    dap.listeners.before.attach.dapui_config = function()
         dapui.open()
     end
-    dap.listeners.before.event_terminated['dapui_config'] = function()
+    dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
         dapui.close()
     end
-    dap.listeners.before.event_exited['dapui_config'] = function()
+    dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
     end
 
