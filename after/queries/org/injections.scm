1d7dc5d82a086d340b8052ebfce212c39f8c740d
Milan Glacier <dev@milanglacier.com>
2024-05-24 21:00:21 -0400

    config(org): update injection for `jupyter-` src block.

--- after/queries/org/injections.scm
+++ after/queries/org/injections.scm
@@ -2,9 +2,15 @@
 
 (block
   parameter: (expr) @_lang
-  (#match? @_lang "^R$")
+  (#match? @_lang "^jupyter-[rR]$")
   (contents) @injection.content
   (#set! injection.include-children)
   (#set! injection.language "r"))
 
  
+(block
+  parameter: (expr) @_lang
+  (#match? @_lang "^jupyter-[pP]ython$")
+  (contents) @injection.content
+  (#set! injection.include-children)
+  (#set! injection.language "python"))
