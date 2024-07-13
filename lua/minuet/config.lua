ebe0782ba17ccec061f89c965e822f04c1017e87
Milan Glacier <dev@milanglacier.com>
2024-07-12 20:57:12 -0400

    config(minuet): update prompt.

--- lua/minuet/config.lua
+++ lua/minuet/config.lua
@@ -60,8 +60,11 @@ Example output:
 ```
 ]]
 
-local claude_guidelines =
-    string.format('%s\n%s', default_guidelines, '6. Keep each completion option concise, limiting it to several lines.')
+local claude_guidelines = string.format(
+    '%s\n%s',
+    default_guidelines,
+    '6. Keep each completion option concise, limiting it to a single line or only a few lines.'
+)
 
 local M = {
     provider = 'codestral',
