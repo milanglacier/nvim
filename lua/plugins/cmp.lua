5771fb4f8b5ada1cae6b31b8ed881fb91d3c01dd
Milan Glacier <dev@milanglacier.com>
2024-08-04 00:57:12 -0400

    config(minuet): update safety settings for Gemini.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -146,6 +146,24 @@ return {
                                 maxOutputTokens = 256,
                                 topP = 0.9,
                             },
+                            safetySettings = {
+                                {
+                                    category = 'HARM_CATEGORY_DANGEROUS_CONTENT',
+                                    threshold = 'BLOCK_NONE',
+                                },
+                                {
+                                    category = 'HARM_CATEGORY_HATE_SPEECH',
+                                    threshold = 'BLOCK_NONE',
+                                },
+                                {
+                                    category = 'HARM_CATEGORY_HARASSMENT',
+                                    threshold = 'BLOCK_NONE',
+                                },
+                                {
+                                    category = 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+                                    threshold = 'BLOCK_NONE',
+                                },
+                            },
                         },
                     },
                     openai = {
