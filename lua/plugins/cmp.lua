667ac19805c625968a5f8d99420f3242f3d0361e
Milan Glacier <dev@milanglacier.com>
2024-12-15 23:47:49 -0500

    refactor(cmp, minuet): move minuet-ai.nvim out of cmp's dependencies.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,4 +1,76 @@
 return {
+    {
+        'milanglacier/minuet-ai.nvim',
+        event = { 'InsertEnter' },
+        config = function()
+            require('minuet').setup {
+                provider = 'gemini',
+                request_timeout = 4,
+                throttle = 2000,
+                virtualtext = {
+                    auto_trigger_ft = { 'python', 'lua' },
+                    keymap = {
+                        accept = '<A-A>',
+                        accept_line = '<A-a>',
+                        prev = '<A-[>',
+                        next = '<A-]>',
+                        dismiss = '<A-e>',
+                    },
+                },
+                notify = 'error',
+                provider_options = {
+                    codestral = {
+                        optional = {
+                            stop = { '\n\n' },
+                            max_tokens = 256,
+                        },
+                    },
+                    gemini = {
+                        optional = {
+                            generationConfig = {
+                                maxOutputTokens = 256,
+                                topP = 0.9,
+                            },
+                            safetySettings = {
+                                {
+                                    category = 'HARM_CATEGORY_DANGEROUS_CONTENT',
+                                    threshold = 'BLOCK_NONE',
+                                },
+                                {
+                                    category = 'HARM_CATEGORY_HATE_SPEECH',
+                                    threshold = 'BLOCK_NONE',
+                                },
+                                {
+                                    category = 'HARM_CATEGORY_HARASSMENT',
+                                    threshold = 'BLOCK_NONE',
+                                },
+                                {
+                                    category = 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+                                    threshold = 'BLOCK_NONE',
+                                },
+                            },
+                        },
+                    },
+                    openai = {
+                        optional = {
+                            max_tokens = 256,
+                            top_p = 0.9,
+                        },
+                    },
+                    openai_compatible = {
+                        api_key = 'FIREWORKS_API_KEY',
+                        end_point = 'https://api.fireworks.ai/inference/v1/chat/completions',
+                        model = 'accounts/fireworks/models/llama-v3p3-70b-instruct',
+                        name = 'Fireworks',
+                        optional = {
+                            max_tokens = 256,
+                            top_p = 0.9,
+                        },
+                    },
+                },
+            }
+        end,
+    },
     { 'hrsh7th/cmp-nvim-lsp' },
     {
         -- 'hrsh7th/nvim-cmp',
@@ -6,77 +78,6 @@ return {
         'iguanacucumber/magazine.nvim',
         name = 'nvim-cmp',
         dependencies = {
-            {
-                'milanglacier/minuet-ai.nvim',
-                config = function()
-                    require('minuet').setup {
-                        provider = 'gemini',
-                        request_timeout = 4,
-                        throttle = 2000,
-                        virtualtext = {
-                            auto_trigger_ft = { 'python', 'lua' },
-                            keymap = {
-                                accept = '<A-A>',
-                                accept_line = '<A-a>',
-                                prev = '<A-[>',
-                                next = '<A-]>',
-                                dismiss = '<A-e>',
-                            },
-                        },
-                        notify = 'error',
-                        provider_options = {
-                            codestral = {
-                                optional = {
-                                    stop = { '\n\n' },
-                                    max_tokens = 256,
-                                },
-                            },
-                            gemini = {
-                                optional = {
-                                    generationConfig = {
-                                        maxOutputTokens = 256,
-                                        topP = 0.9,
-                                    },
-                                    safetySettings = {
-                                        {
-                                            category = 'HARM_CATEGORY_DANGEROUS_CONTENT',
-                                            threshold = 'BLOCK_NONE',
-                                        },
-                                        {
-                                            category = 'HARM_CATEGORY_HATE_SPEECH',
-                                            threshold = 'BLOCK_NONE',
-                                        },
-                                        {
-                                            category = 'HARM_CATEGORY_HARASSMENT',
-                                            threshold = 'BLOCK_NONE',
-                                        },
-                                        {
-                                            category = 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
-                                            threshold = 'BLOCK_NONE',
-                                        },
-                                    },
-                                },
-                            },
-                            openai = {
-                                optional = {
-                                    max_tokens = 256,
-                                    top_p = 0.9,
-                                },
-                            },
-                            openai_compatible = {
-                                api_key = 'FIREWORKS_API_KEY',
-                                end_point = 'https://api.fireworks.ai/inference/v1/chat/completions',
-                                model = 'accounts/fireworks/models/llama-v3p3-70b-instruct',
-                                name = 'Fireworks',
-                                optional = {
-                                    max_tokens = 256,
-                                    top_p = 0.9,
-                                },
-                            },
-                        },
-                    }
-                end,
-            },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
             { 'hrsh7th/cmp-cmdline' },
