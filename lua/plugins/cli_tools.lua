4b6c3800904cf70ad6c8b1e5f906053155b1e913
Milan Glacier <dev@milanglacier.com>
2025-01-01 14:28:54 -0500

    clean up code.

--- lua/plugins/cli_tools.lua
+++ lua/plugins/cli_tools.lua
@@ -2,7 +2,6 @@ local keymap = vim.api.nvim_set_keymap
 local autocmd = vim.api.nvim_create_autocmd
 local my_augroup = require('conf.builtin_extend').my_augroup
 local bufmap = vim.api.nvim_buf_set_keymap
-local command = vim.api.nvim_create_user_command
 
 return {
     -- an installer for a bunch of CLI tools (e.g. LSP, linters)
