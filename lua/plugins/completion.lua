f35b50419706705d372761dac3b11f418ba33422
Milan Glacier <dev@milanglacier.com>
2025-03-28 03:23:59 -0400

    config(minuet): remove configs merged to upstream.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,37 +1,8 @@
-local gemini_prompt = [[
-You are the backend of an AI-powered code completion engine. Your task is to
-provide code suggestions based on the user's input. The user's code will be
-enclosed in markers:
-
-- `<contextAfterCursor>`: Code context after the cursor
-- `<cursorPosition>`: Current cursor location
-- `<contextBeforeCursor>`: Code context before the cursor
-]]
-
-local gemini_few_shots = {}
-
-gemini_few_shots[1] = {
-    role = 'user',
-    content = [[
-# language: python
-<contextBeforeCursor>
-def fibonacci(n):
-    <cursorPosition>
-<contextAfterCursor>
-
-fib(5)]],
-}
-
-local gemini_chat_input_template =
-    '{{{language}}}\n{{{tab}}}\n<contextBeforeCursor>\n{{{context_before_cursor}}}<cursorPosition>\n<contextAfterCursor>\n{{{context_after_cursor}}}'
-
 local M = {
     {
         'milanglacier/minuet-ai.nvim',
         event = { 'InsertEnter' },
         config = function()
-            gemini_few_shots[2] = require('minuet.config').default_few_shots[2]
-
             require('minuet').setup {
                 provider = 'gemini',
                 request_timeout = 2,
@@ -57,13 +28,6 @@ local M = {
                         },
                     },
                     gemini = {
-                        system = {
-                            prompt = gemini_prompt,
-                        },
-                        few_shots = gemini_few_shots,
-                        chat_input = {
-                            template = gemini_chat_input_template,
-                        },
                         optional = {
                             generationConfig = {
                                 maxOutputTokens = 256,
