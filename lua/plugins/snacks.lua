3c7185b8183fed0709893a9fc7058cc648242a63
Milan Glacier <dev@milanglacier.com>
2025-11-21 03:31:01 -0500

    chore(snacks): use our fork until upstream merge our PR.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,13 +1,25 @@
 return {
     {
-        -- NOTE: We deliberately avoid using the higher-order abstractions provided
-        -- by lazy.nvim, such as combining multiple `opts` functions to merge the
-        -- options table that is eventually passed to the `xxx.setup` function, or
-        -- using `keys` to define keybindings. The rationale behind this is to keep
-        -- the configuration independent of any plugin manager's API, ensuring that
-        -- migration to another package manager will be simpler. (If ever required
-        -- in the future, though there are no current plans.)
-        'folke/snacks.nvim',
+        -- NOTE:
+        -- 1. We deliberately avoid using the higher-order abstractions
+        -- provided by lazy.nvim, such as combining multiple `opts` functions
+        -- to merge the options table that is eventually passed to the
+        -- `xxx.setup` function, or using `keys` to define keybindings. The
+        -- rationale behind this is to keep the configuration independent of
+        -- any plugin manager's API, ensuring that migration to another package
+        -- manager will be simpler. (If ever required in the future, though
+        -- there are no current plans.)
+
+
+        -- FIXME:
+        -- 2. We are using our own fork instead of the original upstream
+        -- (folke/snacks.nvim) because our PRs are still awaiting upstream
+        -- approval. Until those are merged, we will continue using our fork. A
+        -- GitHub Action is configured to perform a daily fast-forward merge
+        -- with the upstream repository. This ensures that our fork remains
+        -- consistently up to date.
+        'milanglacier/snacks.nvim',
+        branch = 'dev',
         init = function()
             -- Unlike `opts` (which lazy.nvim merges from multiple functions that
             -- can be defined repeatedly), lazy.nvim permits only a single `init`
