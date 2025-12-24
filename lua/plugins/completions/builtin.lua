22d755d7ad9f4784b55d344a0327c31026ef3d56
Milan Glacier <dev@milanglacier.com>
2025-12-24 02:00:10 -0500

    config(completion/builtin, mini): update cmdline delay.

--- lua/plugins/completions/builtin.lua
+++ lua/plugins/completions/builtin.lua
@@ -52,7 +52,7 @@ autocmd('CmdlineEnter', {
     callback = function()
         require('mini.cmdline').setup {
             autocomplete = {
-                delay = 0.1,
+                delay = 0,
             },
             autopeek = { enable = false },
         }
